3ce276f1e96a9e57820c4f842a3859683555873c
Lingcao Huang <huanglingcao@gmail.com>
2017-03-12 18:32:36 +0800

    print one message

--- hw2/starter.py
+++ hw2/starter.py
@@ -31,6 +31,8 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
+print(args)
+
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
