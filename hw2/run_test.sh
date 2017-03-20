6f558d37de752cf2ae9090f2506e687c7e59115b
Lingcao Huang <huanglingcao@gmail.com>
2017-03-20 12:19:44 +0800

    add run test bash file

--- hw2/run_test.sh
+++ hw2/run_test.sh
@@ -3,3 +3,6 @@
 ./starter.py --epochs 50  > log_eps50.txt
 #mv training_curve_and_accuarcy.jpg training_curve_and_accuarcy_withoutWeightInit.jpg
 
+./test_batch_normalize.sh
+./test_weight_init.sh
+./test_dropout.sh
